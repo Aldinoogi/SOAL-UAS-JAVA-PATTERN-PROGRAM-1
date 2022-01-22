ef615fc272dd7a4ebd2c604b9cbfb1d001d9d65c
Aldinoogi <93021737+Aldinoogi@users.noreply.github.com>
2022-01-22 10:10:39 +0700

    Pattern 1

---
+++ src/Pattern1.java
@@ -0,0 +1,15 @@
+public class Pattern1 {
+	public static void main (String[]args) {
+		int row;
+		int max_row = 5;
+		int star;
+
+		for (row = 1; row <= max_row; row++) {
+			for (star = 1; star<=row; star++) {
+				System.out.print ("* ");
+			}
+
+			System.out.println();
+		}
+	}
+}
